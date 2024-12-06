e31cd9d1628de371339a08d2f35c1a28fc598559
kevinjohnson2 <kdj5304@psu.edu>
2024-12-06 15:05:40 -0500

    Update EmploymentTrendsFiltered.R

--- EmploymentTrendsFiltered.R
+++ EmploymentTrendsFiltered.R
@@ -3,11 +3,15 @@ employment_trends <- read.csv("employment_trends.csv")
 
 employment_trends <- employment_trends %>%
   mutate(REF_DATE = as.character(REF_DATE)) %>%
-  rename("Industry" = North.American.Industry.Classification.System..NAICS., "Employment" = VALUE)
+  rename("Industry" = North.American.Industry.Classification.System..NAICS., 
+         "Employment" = VALUE, 
+         "Region" = GEO,
+         "Date" = REF_DATE
+         )
 
 post_pandemic_employment_trends <- employment_trends %>%
-  filter(REF_DATE >= "2020-01" & REF_DATE <= "2024-12")
+  filter(Date >= "2020-01" & Date <= "2024-12")
 
 pre_pandemic_employment_trends <- employment_trends %>%
-  filter(REF_DATE >= "2015-01" & REF_DATE <= "2019-12")
+  filter(Date >= "2015-01" & Date <= "2019-12")
  
