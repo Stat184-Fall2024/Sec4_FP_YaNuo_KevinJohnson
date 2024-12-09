5a3068b0efe838b6bed907d1304c71739126cc9f
Louis93621 <LouisHsu0621@gmail.com>
2024-12-09 00:43:18 -0500

    Update EmploymentTrendsFiltered(1).R

--- EmploymentTrendsFiltered(1).R
+++ EmploymentTrendsFiltered(1).R
@@ -26,3 +26,28 @@ pre_pandemic_employment_trends <- employment_trends_clean %>%
   filter(Date >= "2015-01" & Date <= "2019-12")
 
 View(employment_trends_clean)
+
+# Pre-Pandemic Stacked Bar Chart
+pre_stacked_data <- pre_pandemic_employment_trends %>%
+  group_by(Industry) %>%
+  summarise(Total_Employment = sum(Employment, na.rm = TRUE)) %>%
+  mutate(Proportion = Total_Employment / sum(Total_Employment))
+
+# Post-Pandemic Stacked Bar Chart
+post_stacked_data <- post_pandemic_employment_trends %>%
+  group_by(Industry) %>%
+  summarise(Total_Employment = sum(Employment, na.rm = TRUE)) %>%
+  mutate(Proportion = Total_Employment / sum(Total_Employment))
+
+# Group regions into categories
+employment_trends_group <- employment_trends_clean %>%
+  mutate(
+    Region_Group = case_when(
+      Region %in% c("British Columbia", "Alberta", "Saskatchewan", "Manitoba") ~ "Western Canada",
+      Region %in% c("Ontario", "Quebec") ~ "Central Canada",
+      Region %in% c("New Brunswick", "Nova Scotia", "Prince Edward Island", "Newfoundland and Labrador") ~ "Atlantic Canada",
+      TRUE ~ "Northern Territories"
+    )
+  )
+
+
